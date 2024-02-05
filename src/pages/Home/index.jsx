6d99f0ae03b5ea7cb847ab3a31455933dd0bd39a
lucasp94 <contato.lucas.calazans@gmail.com>
2024-02-05 15:15:29 -0300

    Create ButtonText

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import {Container, Brand, Menu, Search, Content, NewNote} from "./styles"
 import { Header } from "../../components/Header"
-
+import { ButtonText } from "../../components/ButtonText"
 
 export function Home() {
     return(
@@ -11,7 +11,7 @@ export function Home() {
             <Header />
 
             <Menu>
-
+                <ButtonText/>
             </Menu>
 
             <Search>
